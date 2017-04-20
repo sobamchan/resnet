d662b92c25b2b5a58d195828172a02a12f94cafe
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-04-20 18:47:20 +0900

    plain cnn

---
+++ model.py
@@ -0,0 +1,57 @@
+import numpy as np
+import chainer
+from sobamchan import sobamchan_chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import Variable
+
+class PlainCNN(sobamchan_chainer.Model):
+
+    def __init__(self):
+        super(PlainCNN, self).__init__()
+        layer_i = 1
+        n = 2
+        modules = []
+        input_channel = 3
+        # 32 layer
+        for i in range(layer_i, layer_i+n*2+1):
+            modules += [('conv_{}'.format(layer_i), L.Convolution2D(input_channel, 32, ksize=(3,3), stride=2, pad=4))]
+            modules += [('bnorm_{}'.format(layer_i), L.BatchNormalization(32))]
+            input_channel = 32
+            layer_i += 1
+        # 16 layer
+        for i in range(layer_i, layer_i+n*2):
+            modules += [('conv_{}'.format(layer_i), L.Convolution2D(input_channel, 16, ksize=(3,3), stride=2, pad=4))]
+            modules += [('bnorm_{}'.format(layer_i), L.BatchNormalization(16))]
+            input_channel = 16
+            layer_i += 1
+        # 8 layer
+        for i in range(layer_i, layer_i+n*2):
+            modules += [('conv_{}'.format(layer_i), L.Convolution2D(input_channel, 8, ksize=(3,3), stride=2, pad=4))]
+            modules += [('bnorm_{}'.format(layer_i), L.BatchNormalization(8))]
+            input_channel = 8
+            layer_i += 1
+
+        modules += [('fc', L.Linear(None, 10))]
+        
+        # register
+        [ self.add_link(*link) for link in modules ]
+        self.modules = modules
+        self.layer_n = layer_i
+
+    def __call__(self, x, t, train=True):
+        y = self.fwd(x, train)
+        return F.softmax_cross_entropy(y, t)
+
+    def fwd(self, x, train=True):
+        # convs and bns
+        for i in range(1, self.layer_n-1):
+            x = self['conv_{}'.format(i)](x)
+            x = F.relu(x)
+            x = self['bnorm_{}'.format(i)](x)
+            if i == 1:
+                x = F.average_pooling_2d(x, (2,2))
+        # fc
+        x = self['fc'](x)
+
+        return x
---
+++ train.py
@@ -0,0 +1,66 @@
+import fire
+import numpy as np
+from tqdm import tqdm
+from model import PlainCNN
+import chainer
+from chainer import optimizers
+import chainer.functions as F
+
+from sobamchan.sobamchan_iterator import Iterator
+
+model = PlainCNN()
+
+def train(model=model, gpu=None, epoch=10, batch_size=128):
+    train, test = chainer.datasets.cifar.get_cifar10()
+    train_x = np.array([x[0] for x in train])
+    train_t = np.array([x[1] for x in train])
+    test_x = np.array([x[0] for x in test])
+    test_t = np.array([x[1] for x in test])
+    train_n = len(train_x)
+    test_n = len(test_x)
+    train_x = train_x.reshape(train_n, 3, 32, 32)
+    test_x = test_x.reshape(test_n, 3, 32, 32)
+
+    train_x = np.subtract(train_x, np.mean(train_x, axis=0))
+    test_x = np.subtract(test_x, np.mean(test_x, axis=0))
+
+    optimizer = optimizers.SGD()
+    if gpu:
+        chainer.cuda.get_device(gpu).use()
+        model.to_gpu()
+        xp = chainer.cuda.cupy
+    else:
+        xp = np
+
+    optimizer.setup(model)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(.05))
+    optimizer.add_hook(chainer.optimizer.GradientClipping(.0005))
+
+    for i in tqdm(range(epoch)):
+        order = np.random.permutation(train_n)
+        train_iter_x = Iterator(train_x, batch_size, order=order)
+        train_iter_t = Iterator(train_t, batch_size, order=order)
+        for x, t in tqdm(zip(train_iter_x, train_iter_t), total=train_n/batch_size):
+            x = model.prepare_input(x, dtype=xp.float32, xp=xp)
+            t = model.prepare_input(t, dtype=xp.int32, xp=xp)
+            model.cleargrads()
+            loss = model(x, t)
+            loss.backward()
+            optimizer.update()
+
+        test_iter_x = Iterator(test_x, batch_size, order=order)
+        test_iter_t = Iterator(test_t, batch_size, order=order)
+        print('test starting')
+        for x, t in tqdm(zip(test_iter_x, test_iter_t), total=test_n/batch_size):
+            x = model.prepare_input(x, dtype=xp.float32, xp=xp)
+            t = model.prepare_input(t, dtype=xp.int32, xp=xp)
+            model.cleargrads()
+            y = model.fwd(x, train=False)
+            acc = F.accuracy(y, t)
+            print(acc.data)
+            
+        
+
+
+
+fire.Fire()
